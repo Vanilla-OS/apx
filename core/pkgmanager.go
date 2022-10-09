38687864800d8612645577c1c4077135a3b41bbe
Mirko Brombin <send@mirko.pm>
2022-10-09 23:25:26 +0200

    pkgmanager: Support sudo in the container for pkg manager

--- core/pkgmanager.go
+++ core/pkgmanager.go
@@ -3,15 +3,21 @@ package core
 import "github.com/vanilla-os/apx/settings"
 
 func GetPkgManager(sys bool) []string {
+	sudo := settings.Cnf.PkgManager.Sudo
+
 	if sys {
 		bin := settings.Cnf.PkgManager.Lock
-		sudo := settings.Cnf.PkgManager.Sudo
 
 		if sudo {
 			return []string{"sudo", bin}
 		}
 		return []string{bin}
 	}
+
 	bin := settings.Cnf.PkgManager.Bin
+
+	if sudo {
+		return []string{"sudo", bin}
+	}
 	return []string{bin}
 }
