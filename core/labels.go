93fd8509b2fd49581e9e7429356428a8518cccff
jim3692 <jim3692@gmail.com>
2023-01-10 21:12:38 +0200

    Declare ContainerLabels type

---
+++ core/labels.go
@@ -0,0 +1,23 @@
+package core
+
+import (
+	"fmt"
+
+	"github.com/vanilla-os/apx/settings"
+)
+
+type ContainerLabels struct {
+	Managed    bool
+	Distro     string
+	PkgManager settings.PackageManager
+	Userid     int
+}
+
+func (l *ContainerLabels) ToArguments() []string {
+	return []string{
+		"--label=apx.managed=" + fmt.Sprint(l.Managed),
+		"--label=apx.distro=" + l.Distro,
+		"--label=apx.pkgmanager=" + string(l.PkgManager),
+		"--label=apx.userid=" + fmt.Sprint(l.Userid),
+	}
+}
