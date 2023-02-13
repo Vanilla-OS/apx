c5db35e53d792cc250e2244e9bd0e1d155cf4d39
Brian Ketelsen <bketelsen@gmail.com>
2023-02-13 09:29:22 -0500

    feat: nix search

--- cmd/search.go
+++ cmd/search.go
@@ -9,9 +9,8 @@ package cmd
 */
 
 import (
-	"errors"
-
 	"github.com/spf13/cobra"
+	"github.com/vanilla-os/apx/core"
 	"github.com/vanilla-os/orchid/cmdr"
 )
 
@@ -27,7 +26,8 @@ func NewSearchCommand() *cmdr.Command {
 
 func search(cmd *cobra.Command, args []string) error {
 	if cmd.Flag("nix").Changed {
-		return errors.New(apx.Trans("apx.notForNix"))
+		//return errors.New(apx.Trans("apx.notForNix"))
+		return core.NixSearchPackage(args[0])
 
 	}
 	command := append([]string{}, container.GetPkgCommand("search")...)
